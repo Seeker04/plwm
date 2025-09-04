c35f7a1d951936dcdf752de201448629d6e8ce3f
Skgland <bennet.blessmann+github@googlemail.com>
2025-09-04 21:23:19 +0200

    fix swi some more

--- src/swi/init.pl
+++ src/swi/init.pl
@@ -4,11 +4,16 @@
 % ERROR: '$open_wic'/2: Type error: `option' expected, found `init_file(none)' (a compound)
 % :- set_prolog_flag(iso, true).
 
+% error
+goal_expansion(error(Exception, Src), throw(error(Exception, Src))).
+
+% format
 goal_expansion(compat_format(Fmt, Args), format(Fmt, Args)).
 goal_expansion(compat_format(Stream, Fmt, Args), format(Stream, Fmt, Args)).
 
+
+%iso_ext
 :- meta_predicate(compat_forall(0,0)).
 compat_forall(_,_) :-not_used.
 
-%iso_ext
 goal_expansion(compat_forall(Goal, Test), forall(Module:Goal, Module:Test)) :- prolog_load_context(module, Module).
