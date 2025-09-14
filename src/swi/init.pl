bf1bf3cc08a37322159338dabd89dcb9c1f0bd5d
Skgland <bennet.blessmann+github@googlemail.com>
2025-09-14 18:24:52 +0200

    load init.pl for tests

--- src/swi/init.pl
+++ src/swi/init.pl
@@ -1,4 +1,4 @@
-:- set_prolog_flag(autoload, false).
+% :- set_prolog_flag(autoload, false).
 
 % enabling this causes compilation to fail with the following cryptic error
 % ERROR: '$open_wic'/2: Type error: `option' expected, found `init_file(none)' (a compound)
